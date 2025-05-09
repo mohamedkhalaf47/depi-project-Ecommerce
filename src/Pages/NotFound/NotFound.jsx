6f6c8572d50d6c66c4c34847436c743c38730f8e
Mohamed Khalaf <152212622+mohamedkhalaf47@users.noreply.github.com>
2025-05-09 06:43:19 +0300

    Modified The Error Based on The Token

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,33 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
 import styles from "./NotFound.module.css";
 import { useNavigate } from "react-router-dom";
+import { ShopContext } from "../../Context/ShopContext";
 
 const NotFound = () => {
 	const navigate = useNavigate();
+	const { token } = useContext(ShopContext);
 	return (
-		<div className={`${styles.notFoundContainer} d-flex align-items-center justify-content-center flex-column my-5`}>
-			<h1>404</h1>
-			<h4>Page Not Found</h4>
-			<p>The page you are looking for doesn't exist</p>
+		<div
+			className={`${styles.notFoundContainer} d-flex align-items-center justify-content-center flex-column my-5`}
+		>
+			<h1>{token ? "404" : "401"}</h1>
+			<h4>{token ? "Page Not Found" : "Login To Use Our Features"}</h4>
+			<p>
+				{token
+					? "The page you are looking for doesn't exist"
+					: "Click On The Following Button To Login"}
+			</p>
 			<div className="d-flex gap-2 align-items-center">
-				<button
-					onClick={() => {
-						navigate("/");
-						scrollTo(0, 0);
-					}}
-					className={`btn btn-sm btn-dark mt-3 ${styles.error_button}`}
-				>
-					Back To Home
-				</button>
-				<button
-					onClick={() => {
-						navigate("/Shop-All");
-						scrollTo(0, 0);
-					}}
-					className={`btn btn-sm btn-dark mt-3 ${styles.error_button}`}
-				>
-					View Our Products
-				</button>
+				{token ? (
+					<>
+						<button
+							onClick={() => {
+								navigate("/");
+								scrollTo(0, 0);
+							}}
+							className={`btn btn-sm btn-dark mt-3 ${styles.error_button}`}
+						>
+							Back To Home
+						</button>
+						<button
+							onClick={() => {
+								navigate("/Shop-All");
+								scrollTo(0, 0);
+							}}
+							className={`btn btn-sm btn-dark mt-3 ${styles.error_button}`}
+						>
+							View Our Products
+						</button>
+					</>
+				) : (
+					<>
+						<button
+							onClick={() => {
+								navigate("/login");
+								scrollTo(0, 0);
+							}}
+							className={`btn btn-sm btn-dark mt-3 ${styles.error_button}`}
+						>
+							Login
+						</button>
+					</>
+				)}
 			</div>
 		</div>
 	);
